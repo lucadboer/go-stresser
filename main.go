b6300602944a4fd9d88047386b4ed3468e9504c6
Luca <luca.boer@outlook.com>
2024-04-27 08:48:25 -0300

    fix: lint

--- main.go
+++ main.go
@@ -13,29 +13,29 @@ func main() {
 	requests := flag.Int("requests", 100, "Número total de requests")
 	concurrency := flag.Int("concurrency", 10, "Número de chamadas simultâneas")
 	flag.Parse()
-	
+
 	if *url == "" {
 		fmt.Println("URL é um parâmetro obrigatório")
 		return
 	}
-	
+
 	var totalDuration time.Duration
 	var successRequests int
 	statusCodes := make(map[int]int)
 	var mu sync.Mutex
-	
+
 	doRequest := func(wg *sync.WaitGroup) {
 		defer wg.Done()
-		
+
 		start := time.Now()
 		resp, err := http.Get(*url)
 		duration := time.Since(start)
-		
+
 		mu.Lock()
 		defer mu.Unlock()
-		
+
 		totalDuration += duration
-		
+
 		if err == nil {
 			statusCodes[resp.StatusCode]++
 			if resp.StatusCode == 200 {
@@ -46,13 +46,13 @@ func main() {
 			statusCodes[0]++
 		}
 	}
-	
+
 	var wg sync.WaitGroup
-	
+
 	requestsPerGoroutine := *requests / *concurrency
-	
+
 	extraRequests := *requests % *concurrency
-	
+
 	for i := 0; i < *concurrency; i++ {
 		wg.Add(1)
 		go func(i int) {
@@ -66,14 +66,14 @@ func main() {
 			}
 		}(i)
 	}
-	
+
 	wg.Wait()
-	
+
 	fmt.Printf("Tempo total gasto: %v\n", totalDuration)
 	fmt.Printf("Quantidade total de requests realizados: %d\n", *requests)
 	fmt.Printf("Quantidade de requests com status HTTP 200: %d\n", successRequests)
 	fmt.Println("Distribuição de outros códigos de status HTTP:")
-	
+
 	for code, count := range statusCodes {
 		fmt.Printf("%d: %d\n", code, count)
 	}
